15877beb21193784039747e8f06ec09521438e62
Rodrigo Inácio <rodrigoinco@gmail.com>
2022-07-21 12:02:08 -0300

    Refatorando a classe APP

--- src/App.java
+++ src/App.java
@@ -7,9 +7,16 @@ import java.util.List;
 import java.util.Map;
 
 public class App {
+    // Constantes
+    public static final String WHITE = "\u001b[37m";
+    public static final String BOLD = "\u001b[1m";
+    public static final String RESET = "\u001b[m";
+    public static final String BKMAGENTA = "\u001b[45m";
+    public static final String YELLOW = "\u001b[33m";
+
     public static void main(String[] args) throws Exception {
-        
-        //fazer uma conexão HTTP e buscar os 250 filmes
+
+        // fazer uma conexão HTTP e buscar os 250 filmes
         String url = "https://alura-filmes.herokuapp.com/conteudos";
         URI endereco = URI.create(url);
         HttpClient client = HttpClient.newHttpClient();
@@ -17,26 +24,22 @@ public class App {
         HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
         String body = response.body();
 
-        //extrair só os dados que interessam (título, poster, classificação)
+        // extrair só os dados que interessam (título, poster, classificação)
         var parser = new JsonParser();
         List<Map<String, String>> listaDeFilmes = parser.parse(body);
         System.out.println(listaDeFilmes.size());
-        
-        //exibir e manipular os dados
-        String white = "\u001b[37m";
-        String bold = "\u001b[1m";
-        String reset = "\u001b[m";
-        String backMagenta = "\u001b[45m";
-        String yellowBold = "\u001b[1;33m";
-        String tituloTxt = white.concat("Título: ").concat(bold);
-        String imageTxt = white.concat("Imagem: ").concat(bold);
-        String classificacaoTxt = white.concat(backMagenta).concat("Classificação: ");
 
-        for (Map<String,String> filme : listaDeFilmes) {
-            System.out.println(tituloTxt + filme.get("title") + reset);
-            System.out.println(imageTxt + filme.get("image") + reset);
-            System.out.println(classificacaoTxt + filme.get("imDbRating") + reset);
-            System.out.println(yellowBold + "*".repeat((int) Double.parseDouble(filme.get("imDbRating"))) + reset);
+        // exibir e manipular os dados
+
+        String tituloTxt = WHITE + "Título: " + BOLD;
+        String imageTxt = WHITE + "Imagem: " + BOLD;
+        String classificacaoTxt = WHITE + BKMAGENTA + "Classificação: ";
+
+        for (Map<String, String> filme : listaDeFilmes) {
+            System.out.println(tituloTxt + filme.get("title") + RESET);
+            System.out.println(imageTxt + filme.get("image") + RESET);
+            System.out.println(classificacaoTxt + filme.get("imDbRating") + RESET);
+            System.out.println(YELLOW + BOLD + "*".repeat((int) Double.parseDouble(filme.get("imDbRating"))) + RESET);
         }
     }
 }
